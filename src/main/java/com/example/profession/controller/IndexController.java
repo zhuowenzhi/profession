a0ae63872a0e2df3470feba822b487f072339a39
zhuowenzhi <1791388628@qq.com>
2020-02-20 12:27:40 +0800

    可以运行的项目

--- src/main/java/com/example/profession/controller/IndexController.java
+++ src/main/java/com/example/profession/controller/IndexController.java
@@ -1,14 +1,7 @@
 package com.example.profession.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.swing.text.View;
-
 /**
  * Created by zwz on 2020/02/20
  */
