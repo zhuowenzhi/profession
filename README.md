d7df70a0f65196c69ca4c933f4efb84cf8d47963
zhuowenzhi <1791388628@qq.com>
2020-02-20 21:59:42 +0800

    登录授权完成

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 职业社区
+
+1. github授权登陆
+2. session保持登陆状态
+3. 使用okhttp请求
--- src/main/java/com/example/profession/controller/AuthorizeController.java
+++ src/main/java/com/example/profession/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Author: zwz
  * Date: 2020/02/20
@@ -31,7 +33,9 @@ public class AuthorizeController {
 
 
     @GetMapping("/callback")
-    public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state) {
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
 
         AccessTokenDTO accessTokenDTO =new AccessTokenDTO();
         accessTokenDTO.setCode(code);
@@ -42,8 +46,14 @@ public class AuthorizeController {
 
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
 
-        return "index";
+        if (user != null) {
+            //登录成功.写cookie和session
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
+        } else {
+            //登录失败
+            return "redirect:/";
+        }
     }
 }
