b52b864b5fffe0d92ec1993a86ed88caae279b59
zhuowenzhi <1791388628@qq.com>
2020-02-21 16:31:19 +0800

    数据库创建

---
+++ src/main/java/com/example/profession/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.example.profession.mapper;
+
+import com.example.profession.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author : zwz
+ * @date 2020/2/20 0020 22:50
+ */
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId),#{token},#{gmtCreate},#{gmtModified}")
+    void insert(User user);
+
+}
