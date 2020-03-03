b886407104448dd7f99333ab9df3fa08fc87fc2b
zhuowenzhi <1791388628@qq.com>
2020-03-04 00:10:32 +0800

    增加事务

--- src/main/java/com/example/profession/service/CommentService.java
+++ src/main/java/com/example/profession/service/CommentService.java
@@ -10,6 +10,7 @@ import com.example.profession.model.Comment;
 import com.example.profession.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author : zwz
@@ -27,6 +28,7 @@ public class CommentService {
     @Autowired
     private QuestionExtMapper questionExtMapper;
 
+    @Transactional
     public void insert(Comment comment) {
         if (comment.getParentId() == null || comment.getCommentator() == 0) {
            throw new CustomizeException(CustomizeErrorCode.TARGET_PARAM_NOT_FOUND);
